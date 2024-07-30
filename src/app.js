5bcf2a4bef11668f800047976f32c44ddc0b6e19
Valentina Rodriguez Chia <valentinarchia@gmail.com>
2024-07-30 03:01:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "My sister", "My cat"];
+  let action = [" ate", " peed", " smash", " broke"];
+  let what = [" my homework", " my phone", " the car", " my computer"];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was at the mall",
+    " during my lunch",
+    " while I was with my mom"
+  ];
+
+  let parte1 = Math.floor(Math.random() * who.length);
+  let parte2 = Math.floor(Math.random() * action.length);
+  let parte3 = Math.floor(Math.random() * what.length);
+  let parte4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[parte1] + action[parte2] + what[parte3] + when[parte4];
 };
